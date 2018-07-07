63c3a67507e6caf5abe991020655d7d8cf4c08b6
etridenour <etridenour@gmail.com>
2018-07-06 21:34:14 -0500

    working version

--- object-exercises/RPG-game-2.py
+++ object-exercises/RPG-game-2.py
@@ -21,6 +21,7 @@ class Character:
         hero_damage_multiplier = 1
         enemy_damage_multiplier = 1
         rand_heal = random.randint(1,5)
+        armor_multiplier = 1
         if enemy.name == 'Shadow':
             rand_evade = random.randint(1, 10)
             if rand_evade > 1:
@@ -28,45 +29,47 @@ class Character:
                 hero_damage_multiplier = 0
                 
             else:
-                enemy.health -= self.power
-        if enemy.name == 'Goblin':
+                pass
+        elif enemy.name == 'Goblin':
             rand_block = random.randint(1, 3)
             if rand_block == 1:
                 print('The {} blocked {}\'s attack.'.format(enemy.name, self.name))
                 hero_damage_multiplier = 0
             else:
-                enemy.health -= self.power
+                pass
 
-        if enemy.name == 'Sharknado':
+        elif enemy.name == 'Sharknado':
             rand_sweep = random.randint(1,4)
             if rand_sweep == 1:
                 print("The {} swept up {}.".format(enemy.name, self.name))
                 enemy_damage_multiplier = 5
             else:
-                enemy.health -= self.power
-
-
-        if enemy.name == 'Zombie':
-            enemy.health -= self.power
+                pass
             
-
-
-        if enemy.name == "Angry Beaver":
+        elif enemy.name == "Angry Beaver":
             rand_angry = random.randint(1,3)
             if rand_angry == 1:
                 print('The {} is super angry!'.format(enemy.name))
                 enemy_damage_multiplier = 2
             else:
-                enemy.health -= self.power
+                pass
 
         rand_attack = random.randint(1,5)
         if hero_damage_multiplier == 0:
             pass
+        elif self.sword > 0:
+            hero_damage_multiplier = 3
+            print('{} attacked {} with the sword for triple power!'.format(self.name, enemy.name))
+            self.sword -= 1
+        elif self.bazooka > 0:
+            hero_damage_multiplier = 25
+            print('{} blew up {} with the bazooka!'.format(self.name, enemy.name))
+            self.bazooka -= 1
         elif rand_attack == 1:
             hero_damage_multiplier = 2
-            print('The {} hit with double power!'.format(self.name))
-            enemy.health -= self.power * hero_damage_multiplier
-
+            print('{} attacked {} with double power!'.format(self.name, enemy.name))
+            
+        enemy.health -= self.power * hero_damage_multiplier
         print("{} does {} damage to the {}.".format(self.name, self.power * hero_damage_multiplier, enemy.name))
         if enemy.health <= 0:
             if enemy.name == 'Goblin':
@@ -76,29 +79,57 @@ class Character:
                 print("{} is rewarded with a bounty of {} coins for slaying the evil {}.".format(self.name, enemy.bounty, enemy.name))
                 print()
                 print('Shadow quickly approches to battle!')
-            if enemy.name == 'Shadow':
+            elif enemy.name == 'Shadow':
                 print("The {} is dead.".format(enemy.name))
                 print('It\'s body vaporizes into into thin air.')
                 self.coins += enemy.bounty
                 print("{} is rewarded with a bounty of {} coins for slaying the evil {}.".format(self.name, enemy.bounty, enemy.name))
                 print()
                 print('The zombie slowly stumbles up for an attack!')
-            if enemy.name == 'Sharknado':
+            elif enemy.name == 'Sharknado':
                 print("{} is dead.".format(enemy.name))
                 print('It dissipates high in the sky, launching sharks all over the place.')
                 self.coins += enemy.bounty
                 print("{} is rewarded with a bounty of {} coins for slaying the evil {}.".format(self.name, enemy.bounty, enemy.name))
                 print()
                 print('Emerging from a nearby dam comes an Angry Beaver, the final enemy!')
-        elif enemy.name == 'Zombie':
-            print("{} counters with {} damage to {}.".format(enemy.name, enemy_damage_multiplier * enemy.power, self.name))
-            self.health -= enemy_damage_multiplier * enemy.power
-            if enemy.health <= 0:
-                print("You can't kill the {}!".format(enemy.name))
+            elif enemy.name == 'Zombie':
+                print("{} counters with {} damage to {}.".format(enemy.name, enemy_damage_multiplier * enemy.power, self.name))
+                self.health -= enemy_damage_multiplier * enemy.power
+                if enemy.health <= 0:
+                    print("You can't kill the {}!".format(enemy.name))
 
         else:
-            print("{} counters with {} damage to {}.".format(enemy.name, enemy_damage_multiplier * enemy.power, self.name))
-            self.health -= enemy_damage_multiplier * enemy.power
+            if self.evade > 5:
+                hero_evade_1 = random.randint(1, 10)
+                if hero_evade_1 == 1:
+                    print('{} makes an evasive maneuver!'.format(self.name))
+                    enemy_damage_multiplier = 0
+                else:
+                    pass
+            if self.evade > 3:
+                hero_evade_2 = random.randint(1, 5)
+                if hero_evade_2 == 1:
+                    print('{} makes an evasive maneuver!'.format(self.name))
+                    enemy_damage_multiplier = 0
+            if self.evade > 1:
+                hero_evade_3 = random.randint(1, 3)
+                if hero_evade_3 == 1:
+                    print('{} makes an evasive maneuver!'.format(self.name))
+                    enemy_damage_multiplier = 0
+                else:
+                    pass
+            else:
+                pass
+
+            if self.armor > 1:
+                armor_multiplier = .90
+            elif self.armor > 3:
+                armor_multiplier = .80
+            elif self.armor > 5:
+                armor_multiplier = .70
+            print("{} counters with {} damage to {}.".format(enemy.name, enemy_damage_multiplier * enemy.power * armor_multiplier, self.name))
+            self.health -= (enemy_damage_multiplier * enemy.power * armor_multiplier)
 
         if self.health <= 0:
             print("{} is dead.".format(self.name))
@@ -108,10 +139,6 @@ class Character:
             self.health += 2
             print('{} was healed +2 by the medic. What a nice medic.'.format(self.name))
             
-            
-
-
-
     def alive(self):
         if self.name == "Zombie":
             return True
@@ -125,45 +152,97 @@ class Character:
 
 
 class Hero(Character):
-    def __init__(self, power, health, name, coins, armor):
+    def __init__(self, power, health, name, coins, armor, evade, sword, bazooka):
         super().__init__(power, health, name)
         self.coins = coins
-        
+        self.armor = armor
+        self.evade = evade
+        self.sword = sword
+        self.bazooka = bazooka
     
-class Tonic:
-        cost = 5
-        name = 'Tonic'
-        def use(self, hero):
-            hero.health = 10
-            print('{} has been restored to full health.'.format(hero.name))
-            
-class Armor:
-        cost = 5
-        name = 'Armor'
-        def use(self, hero):
-            hero.armor = 2
+    
+    def print_status(self):
+        print("{} has {} health, {} power, {} armor, {} evade, {} sword, and {} bazooka.".format(self.name, self.health, self.power, self.armor, self.evade, self.sword, self.bazooka))
 
-class Evade:
-    cost = 5
-    name ='Evade'
-    def use(self, hero):
-        hero.evade = 2
-
-def buy_stuff(self):
-    while True:
+    def buy_stuff(self):
+        print()
         print('Welcome to the store.')
-        print('You have {} coins.'.format(hero.coins))
-        print()
-        print('We have:')
-        print('1. Tonic - bring health back to full strength.')
-        print('2. Armor - increase ability to receive damage')
-        print('3. Evade - increase ability to evade enemy attack')
-        print('4. Energy Sword - increase attack strength')
-        print('5. Bazooka - blow up your enemy')
-        print()
-        print('What would you like to purchase? (1-5)')
-        choice = input('>')
-            
+        while self.coins > 0:
+            print()
+            print('You have {} coins.'.format(hero.coins))
+            print()
+            print('Items for sale:')
+            print('1. Tonic (5 coins)- bring health back to full strength.')
+            print('2. Armor (5 coins) - increase ability to receive damage')
+            print('3. Evade (5 coins)- increase ability to evade enemy attack')
+            print('4. Energy Sword (10 coins)- increase attack strength by 3 times (one time use)')
+            print('5. Bazooka (15 coins) - blow up your enemy (one time use)')
+            print('6. Exit')
+            print()
+            print('What would you like to purchase? (1-6)')
+            choice = int(input('> '))
+            print()
+            if choice == 1:
+                hero.use_tonic()
+            elif choice == 2:
+                hero.use_armor()
+            elif choice == 3:
+                hero.use_evade()
+            elif choice == 4:
+                hero.use_sword()
+            elif choice == 5:
+                hero.use_bazooka()
+            elif choice == 6:
+                break
+        else:
+            print('You have no coins.')
+            print('Goodbye.')
+            print()
+        
+    def use_tonic(self):
+        if self.coins < 5:
+            print('You don\'t have enough coins.')
+        else:
+            hero.health = 10
+            self.coins -= 5
+            print('{} has been restored to full health.'.format(hero.name))
+            self.print_status()
+
+    def use_armor(self):  
+        if self.coins < 5:
+            print('You don\'t have enough coins.')      
+        else:
+            self.coins -= 5
+            self.armor += 2
+            print('{} now has +2 armor.'.format(hero.name))
+            self.print_status()
+
+    def use_evade(self):
+        if self.coins < 5:
+            print('You don\'t have enough coins.') 
+        else:
+            self.coins -= 5
+            self.evade += 2
+            print('{} now has +2 evade.'.format(hero.name))
+            self.print_status()
+
+    def use_sword(self):
+        if self.coins < 10:
+            print('You don\'t have enough coins.') 
+        else:
+            self.coins -= 10
+            self.sword += 1
+            print('{} now has a sword.'.format(hero.name))
+            self.print_status()
+
+    def use_bazooka(self):
+        if self.coins < 15:
+            print('You don\'t have enough coins.') 
+        else:
+            self.coins -= 15
+            self.bazooka += 1
+            print('{} now has a bazooka.'.format(hero.name))
+            self.print_status()
 
     # def attack(self, enemy):
     #     rand_attack = random.randint(1,5)
@@ -211,35 +290,15 @@ class Angry_beaver(Character):
         super().__init__(power, health, name)
         self.bounty = 7
 
-# class Store:
-#     def __init__(self, tonic, armor, evade, sword, bazooka):
-#         self.tonic = tonic
-#         self.armor = armor
-#         self.evade = evade
-#         self.sword = sword
-#         self.bazooka = bazooka
 
-#     def buy_stuff(self):
-#             print('Welcome to the store.')
-#             print()
-#             print('We have:')
-#             print('1. Tonic - bring health back to full strength.')
-#             print('2. Armor - increase ability to receive damage')
-#             print('3. Evade - increase ability to evade enemy attack')
-#             print('4. Energy Sword - increase attack strength')
-#             print('5. Bazooka - blow up your enemy')
-#             print()
-#             print('What would you like to purchase? (1-5)')
-#             choice = input('>')
-
-
-hero = Hero(5, 50, 'Galactar', 20, 0)
+hero = Hero(5, 10, 'Galactar', 20, 0, 0, 0, 0)
 goblin = Goblin(2, 10, "Goblin", 5)
 shadow = Shadow(3, 1, "Shadow", 7)
-zombie = Zombie(2, 5, 'Zombie')
-sharknado = Sharknado(5, 17, 'Sharknado', 12)
-angry_beaver = Angry_beaver(3, 12, "Angry Beaver", 5)
+zombie = Zombie(2, 6, 'Zombie')
+sharknado = Sharknado(5, 15, 'Sharknado', 12)
+angry_beaver = Angry_beaver(3, 20, "Angry Beaver", 5)
 
+print()
 print('********************************************************')
 print("Galactar\'s Hardest Mission - Escape from Beaver Lodge")
 print('********************************************************')
@@ -274,7 +333,7 @@ def main(hero, enemy):
                 enemy = sharknado
 
             else:
-                print("The {} runs away. Not much of a hero.".format(hero.name))
+                print("{} runs away. Not much of a hero.".format(hero.name))
                 print("GAME OVER")
                 break
             
@@ -297,10 +356,10 @@ def main(hero, enemy):
         if sharknado.health <= 0:
             enemy = angry_beaver
         if angry_beaver.health <= 0:
-            print("{} deafeated the powerful Angry Beaver. You win!".format(hero.name))
+            print("{} deafeated the powerful Angry Beaver.".format(hero.name))
+            print('YOU WIN!')
             break
         
-
 main(hero, goblin)
 
 
